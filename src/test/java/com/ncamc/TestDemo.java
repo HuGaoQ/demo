de0dfc86f4651d07d548d75dd9477e77d0953662
hugaoqiang <15629526027@163.com>
2025-02-19 17:32:14 +0800

    1.解决依赖冲突  2.解决snakeyaml升级到2.0启动报错无参问题

--- src/test/java/com/ncamc/TestDemo.java
+++ src/test/java/com/ncamc/TestDemo.java
@@ -2,6 +2,7 @@ package com.ncamc;
 
 import com.ncamc.service.TransactionService;
 import com.ncamc.service.myMethodService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TestDemo {
@@ -21,9 +23,11 @@ public class TestDemo {
 
     @Test
     public void demo() {
+        long startTime = System.currentTimeMillis();
         myMethodService.myMethod();
         transactionService.transferMoney("AccountA", "AccountB", 500);
-        transactionService.transferMoney("AccountA", "AccountB", 1500);
+        transactionService.transferMoney("AccountA", "AccountB", 100);
+        log.info("执行时间：{}", (System.currentTimeMillis() - startTime) / 1000);
     }
 
 }
